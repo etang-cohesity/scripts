aac245fc3b6436de61d033ccc62648bb12bc01fc
Eddie Tang <57685836+etang-cohesity@users.noreply.github.com>
2019-12-18 18:43:47 -0800

    working runningJobs.csv

--- powershell/runningJobs/runningJobs.ps1
+++ powershell/runningJobs/runningJobs.ps1
@@ -26,29 +26,20 @@ foreach ($run in $runs){
    $startTime = usecsToDate $runStartTime
    $JID = $run.JobId
    $jRunID = $run.backupRun.jobRunId
-   "$run" | Out-File -FilePath ./object.csv -Append
     foreach ($copyRun in $run.copyRun){
         if ($copyRun.status -notin $finishedStates){
             $overallstatus = $null
+            $cjob = $null
             $targetType = $copyRun.target.type.substring(1)
             $status = $copyRun.status.substring(1)
             "{0,-20} {1,-22} {2,-10} {3}" -f ($jobName, $startTime, $targetType, $status)
             "$jobName, $startTime, $targetType, $status" | Out-File -FilePath ./runningJobs.csv -Append
-            #"$copyRun" | Out-File -FilePath ./object.csv -Append
             $cjob = @{
-                "copyTaskUid" = @(
-                    @{
-                        "clusterId"             = $copyRun.taskUid.clusterId;
-                        "clusterIncarnationId"  = $copyRun.taskUid.clusterIncarnationId;
-                        "id"                    = $copyRun.taskUid.id;
-                    }
-                )
                 "jobRunId"= $jRunID;
             }
             Write-Host "Cancelling Protection Job:" $jobName
             $cjob | Out-File -FilePath ./cjob.json -Append
-            $cjob.copyTaskUid | Out-File -FilePath ./cjobtask.json -Append
-            $null = api post "protectionRuns/cancel/$JID"
+            $null = api post "protectionRuns/cancel/$JID" $cjob
         }
     }
 }
